d5706902004f0e824a87646e760fd0425345b9b1
Maximilian Starke <maximilian.starke@medionmail.com>
2021-06-23 16:09:24 +0200

    remove double

--- tools/collect-data-controller/controller.cpp
+++ tools/collect-data-controller/controller.cpp
@@ -189,7 +189,6 @@ int main(int argc, char** argv)
 	std::string command_copy_model = (std::string("cp ") + original_model_path.string() + " " + (artifact_path / ORIGINAL_MODEL_FILE_NAME).string() + logs.write_next());
 	system(command_copy_model.c_str());
 	logs.print_last_log();
-	system((std::string("ls ") + artifact_path.string() + logs.write_next()).c_str());
 	standard_logger().info("Check directory content...");
 	system((std::string("ls -la ") + artifact_path.string() + logs.write_next()).c_str());
 	logs.print_last_log();
