0efeaaf5fc3f7349b73af2ade4b5fc0ef4badf47
Maximilian Starke <maximilian.starke@medionmail.com>
2021-06-23 14:49:01 +0200

    no idea what the error is

--- tools/collect-data-controller/controller.cpp
+++ tools/collect-data-controller/controller.cpp
@@ -183,11 +183,11 @@ int main(int argc, char** argv)
 	system((std::string("mkdir ") + artifact_path.string()).c_str());
 	standard_logger().info("Copying original model...");
 	std::string command_copy_model = (std::string("cp ") + original_model_path.string() + " " + (artifact_path / ORIGINAL_MODEL_FILE_NAME).string() + logs.write_next());
-	standard_logger().info("#1...");
+	std::cout << "#1...";
 	system(command_copy_model.c_str());
-	standard_logger().info("#2...");
+	std::cout << "#2...";
 	logs.print_last_log();
-	standard_logger().info("#3...");
+	std::cout << "#3...";
 	std::cout << "number i : " << logs.ii();
 	standard_logger().info("Check directory content...");
 	system((std::string("ls ") + artifact_path.string() + logs.write_next()).c_str());
