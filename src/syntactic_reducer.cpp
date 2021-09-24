72d574a103e0f000fa533fc76221ba7c3e30fd20
Maximilian Starke <maximilian.starke@medionmail.com>
2021-09-24 10:58:28 +0200

    try fix a mac cloud build error: forgot to explicitly specify ordering predicate for less when using std::sort

--- src/syntactic_reducer.cpp
+++ src/syntactic_reducer.cpp
@@ -480,7 +480,7 @@ void helper_process_sub_colorings(
 		ss << "add node : " << id << " forbid: " << forbidden_merges;
 		standard_logger().info(ss.str());
 	}
-	std::sort(all_sets.back().begin(), all_sets.back().end());
+	std::sort(all_sets.back().begin(), all_sets.back().end(), COMP_BITSET);
 
 	std::size_t next_free_index{ 2 };
 
