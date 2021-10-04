3aa5454e326282c3e2768ca6c815d9d90338fccf
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-04 23:32:54 +0200

    try upload generated models

---
+++ script/ci/upload-generated-models.sh
@@ -0,0 +1,12 @@
+git_username=${1}
+git_access_token=${2}
+
+git_hash_last_commit=$(git rev-parse HEAD)
+git_branch_for_results="ci-generate-models-${git_hash_last_commit}"
+git branch ${git_branch_for_results}
+cp script/ci/azure-yml/azure-pipelines.yml azure-pipelines.yml
+git add *
+git status
+git -c user.name="CI for Necktschnagge" -c user.email="ci-for-necktschnagge@example.org" commit -m "Automatic upload of generated models"
+git status
+git push https://${git_username}:${git_access_token}@github.com/${user_repo_id} ${git_branch_artifacts}
