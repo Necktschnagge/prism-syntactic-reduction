c954fea4218945bc90a1b77362602b62705eb523
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-04 23:56:48 +0200

    add missing variable values for script

--- script/ci/upload-generated-models.sh
+++ script/ci/upload-generated-models.sh
@@ -3,10 +3,12 @@ git_access_token=${2}
 
 git_hash_last_commit=$(git rev-parse HEAD)
 git_branch_for_results="ci-generate-models-${git_hash_last_commit}"
+user_repo_id=$(echo "${git_repo_url}" | sed -E 's/https:\/\/\w*.\w*\///' | sed -E 's/\.git//')
+
 git switch -c ${git_branch_for_results} #switch to new branch pointing to current HEAD
 cp script/ci/azure-yml/azure-pipelines.yml azure-pipelines.yml
 git add *
 git status
 git -c user.name="CI for Necktschnagge" -c user.email="ci-for-necktschnagge@example.org" commit -m "Automatic upload of generated models"
 git status
-git push https://${git_username}:${git_access_token}@github.com/${user_repo_id} ${git_branch_artifacts}
+git push https://${git_username}:${git_access_token}@github.com/${user_repo_id} ${git_branch_for_results}
