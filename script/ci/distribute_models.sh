730edbd26b3b571141eb4092141040d6f3442826
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-05 17:32:17 +0200

    try run distribute script

--- script/ci/distribute_models.sh
+++ script/ci/distribute_models.sh
@@ -20,12 +20,18 @@ user_repo_id=$(echo "${git_repo_url}" | sed -E 's/https:\/\/\w*.\w*\///' | sed -
 cd RESULTS
 for D in `find . -type d`
 do
-	#git switch -c ${git_branch_for_results} #switch to new branch pointing to current HEAD
-	#cp script/ci/azure-yml/run-prism-on-one-model.yml azure-pipelines.yml
-    #cp D ../prism_model
-	#git add *
-	#git status
+	number=$(echo ${D} | sed -E 's/.\///')
+	git switch -c ${branch_name}-${number} #switch to new branch pointing to current HEAD
+	cp script/ci/azure-yml/run-prism-on-one-model.yml azure-pipelines.yml
+    cp D ../prism_model
+	git add *
+	git status
+	git -c user.name="CI for Necktschnagge" -c user.email="ci-for-necktschnagge@example.org" commit -m "Automatic upload of generated models"
+	git status
+	git push https://${git_username}:${git_access_token}@github.com/${user_repo_id} ${git_branch_for_results}
+	git reset --hard HEAD~1
 	echo $D
+	echo $ number
 	
 	
 done
