323fbcecf46852406c70c4cca005126681ecf628
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-05 17:21:00 +0200

    distribution script

--- script/ci/distribute_models.sh
+++ script/ci/distribute_models.sh
@@ -1,11 +1,12 @@
 git_username=${1}
 git_access_token=${2}
-branch_name=${3}
+branch_name=$(echo ${3} | sed -E 's/refs\/heads\///')
 
 git_hash_last_commit=$(git rev-parse HEAD)
 git_branch_for_results="ci-generate-models-${git_hash_last_commit}"
 git_repo_url=$(git remote get-url origin)
 user_repo_id=$(echo "${git_repo_url}" | sed -E 's/https:\/\/\w*.\w*\///' | sed -E 's/\.git//')
+ 
 
 #git switch -c ${git_branch_for_results} #switch to new branch pointing to current HEAD
 #cp script/ci/azure-yml/distribute-generated-models.yml azure-pipelines.yml
@@ -24,7 +25,7 @@ do
     #cp D ../prism_model
 	#git add *
 	#git status
-	echo D
+	echo $D
 	
 	
 done
