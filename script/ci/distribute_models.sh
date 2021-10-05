78dd56bb2a4a938bb2499452bf41096a3500650b
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-05 16:50:38 +0200

    test bash for loop what is D

--- script/ci/distribute_models.sh
+++ script/ci/distribute_models.sh
@@ -1,23 +1,31 @@
 git_username=${1}
 git_access_token=${2}
+branch_name=${3}
 
 git_hash_last_commit=$(git rev-parse HEAD)
 git_branch_for_results="ci-generate-models-${git_hash_last_commit}"
 git_repo_url=$(git remote get-url origin)
 user_repo_id=$(echo "${git_repo_url}" | sed -E 's/https:\/\/\w*.\w*\///' | sed -E 's/\.git//')
 
-git switch -c ${git_branch_for_results} #switch to new branch pointing to current HEAD
-cp script/ci/azure-yml/distribute-generated-models.yml azure-pipelines.yml
-git add *
-git status
-git -c user.name="CI for Necktschnagge" -c user.email="ci-for-necktschnagge@example.org" commit -m "Automatic upload of generated models"
-git status
-git push https://${git_username}:${git_access_token}@github.com/${user_repo_id} ${git_branch_for_results}
+#git switch -c ${git_branch_for_results} #switch to new branch pointing to current HEAD
+#cp script/ci/azure-yml/distribute-generated-models.yml azure-pipelines.yml
+#git add *
+#git status
+#git -c user.name="CI for Necktschnagge" -c user.email="ci-for-necktschnagge@example.org" commit -m "Automatic upload of generated models"
+#git status
+#git push https://${git_username}:${git_access_token}@github.com/${user_repo_id} ${git_branch_for_results}
 
 
-
+cd RESULTS
 for D in `find . -type d`
 do
-    //Do whatever you need with D
+	#git switch -c ${git_branch_for_results} #switch to new branch pointing to current HEAD
+	#cp script/ci/azure-yml/run-prism-on-one-model.yml azure-pipelines.yml
+    #cp D ../prism_model
+	#git add *
+	#git status
+	echo D
+	
+	
 done
 
