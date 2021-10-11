da8f99272db7d76caf1b726f729decc6e1dcf0a3
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-11 12:41:27 +0200

    fix delte branches script

--- script/ci/delete-ci-branches.sh
+++ script/ci/delete-ci-branches.sh
@@ -1,13 +1,16 @@
 remote_name=${1}
+echo ${remote_name}
 
-if [[ ${remote_name} -eq ""]]; then
+if [[ -z "${remote_name}" ]]; then
 	remote_name=origin
 fi
-
+echo ${remote_name}
+git fetch
+#git branch -r | grep "${remote_name}/ci-generate-models-"
 git branch -r | grep ${remote_name}/ci-generate-models- | while read line 
 do
 	echo Delte branch: ${line}
-	branch_name=$(echo ${line} | sed -E 's/${remote_name}\///')
+	branch_name=$(echo ${line} | sed -E "s/${remote_name}\///")
 	echo pure branch name: ${branch_name}
-	#git push ${remote_name} --delete ${branch_name}
+	git push ${remote_name} --delete ${branch_name}
 done
