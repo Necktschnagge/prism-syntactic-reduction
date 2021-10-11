086beea02c1fe5fa2c8e275c523b91a4fa1a2d76
Maximilian Starke <maximilian.starke@medionmail.com>
2021-10-11 12:19:02 +0200

    omit critical branch delete command

--- script/ci/delete-ci-branches.sh
+++ script/ci/delete-ci-branches.sh
@@ -3,5 +3,5 @@ do
 	echo Delte branch: ${line}
 	branch_name=$(echo ${line} | sed -E 's/origin\///')
 	echo pure branch name: ${branch_name}
-	git push origin --delete ${branch_name}
+	#git push origin --delete ${branch_name}
 done
